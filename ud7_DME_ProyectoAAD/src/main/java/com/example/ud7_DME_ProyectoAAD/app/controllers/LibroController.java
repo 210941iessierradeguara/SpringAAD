9385885fae4e1f10f863f8cc49ce24850b24d7df
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2023-03-02 10:14:35 +0100

    2023-03-02 Final clase AAD Jueves

--- ud7_DME_ProyectoAAD/src/main/java/com/example/ud7_DME_ProyectoAAD/app/controllers/LibroController.java
+++ ud7_DME_ProyectoAAD/src/main/java/com/example/ud7_DME_ProyectoAAD/app/controllers/LibroController.java
@@ -1,5 +1,7 @@
 package com.example.ud7_DME_ProyectoAAD.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.ud7_DME_ProyectoAAD.app.models.dao.ILibroDao;
+import com.example.ud7_DME_ProyectoAAD.app.models.entity.Libro;
 
 @Controller
 @SessionAttributes("libro")
@@ -22,5 +25,14 @@ public class LibroController {
 		model.addAttribute("encabezado", "Listado de libros");
 		model.addAttribute("libros", iLibroDao.findAll());
 		return "listar";
+	}
+	
+	@RequestMapping(value = "/form")
+	public String crear(Map<String, Object> model) {
+
+		Libro libro = new Libro();
+		model.put("libro", libro);
+		model.put("encabezado", "Formulario de Libro");
+		return "form";
 	}
 }

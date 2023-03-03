c75a2ef8ec68cb8c56e735dab3acfd6f5db023ca
DAM2Alu5 <DAM2Alu5@B01-PC11.IESGUARA.LOCAL>
2023-03-03 14:17:11 +0100

    2023-03-03 14:17 Final de clase

--- ud7_DME_ProyectoAAD/src/main/java/com/example/ud7_DME_ProyectoAAD/app/controllers/LibroController.java
+++ ud7_DME_ProyectoAAD/src/main/java/com/example/ud7_DME_ProyectoAAD/app/controllers/LibroController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -43,12 +44,36 @@ public class LibroController {
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid Libro libro, BindingResult result, Model model, SessionStatus status) {
 		if(result.hasErrors()) {
-			model.addAttribute("titulo", "Formulario del Libro");
+			model.addAttribute("encabezado", "Formulario del Libro");
 			return "form";
 		}
 		
 		iLibroDao.save(libro);
 		status.setComplete();
 		return "redirect:listar";
+	}
+	
+	@RequestMapping(value="/form/{id}")
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+		
+		Libro libro = null;
+		
+		if(id > 0) {
+			libro = iLibroDao.findOne(id);
+		} else {
+			return "redirect:/listar";
+		}
+		model.put("libro", libro);
+		model.put("encabezado", "Editar Libro");
+		return "form";
+	}
+	
+	@RequestMapping(value="/eliminar/{id}")
+	public String eliminar(@PathVariable(value="id") Long id) {
+		
+		if(id > 0) {
+			iLibroDao.delete(id);
+		}
+		return "redirect:/listar";
 	}
 }
